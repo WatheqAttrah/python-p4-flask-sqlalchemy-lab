5e30827eb9080b37aacc92a5fa810da3f8d62131
Watheq Attrah <novaatrah@gmail.com>
2023-09-05 18:01:38 -0700

    Not Easy for sure

--- server/app.py
+++ server/app.py
@@ -6,29 +6,40 @@ from flask_migrate import Migrate
 from models import db, Zookeeper, Enclosure, Animal
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return '<h1>Zoo app</h1>'
+    return "<h1>Zoo app</h1>"
 
-@app.route('/animal/<int:id>')
+
+@app.route("/animal/<int:id>")
 def animal_by_id(id):
-    return ''
+    animal = Animal.query.filter_by(id=id).first()
+    return f"<ul>ID: {id}</ul><ul>Name: {animal.name}</ul><ul>Zookeeper: {animal.zookeeper.name}</ul><ul>Species: {animal.species}</ul><ul>Enclosure: {animal.enclosure.environment}</ul>"
 
-@app.route('/zookeeper/<int:id>')
+
+@app.route("/zookeeper/<int:id>")
 def zookeeper_by_id(id):
-    return ''
+    zookeeper = Zookeeper.query.filter_by(id=id).first()
+    animal_list = [f"<ul>Animal: {animal.name}</ul>" for animal in zookeeper.animals]
+    formatted_list = "\n".join(animal_list)
+    return f"<ul>ID: {id}</ul><ul>Name: {zookeeper.name}</ul><ul>Birthday: {zookeeper.birthday}</ul>{formatted_list}"
 
-@app.route('/enclosure/<int:id>')
+
+@app.route("/enclosure/<int:id>")
 def enclosure_by_id(id):
-    return ''
+    enclosure = Enclosure.query.filter_by(id=id).first()
+    animal_list = [f"<ul>Animal: {animal.name}</ul>" for animal in enclosure.animals]
+    formatted_list = "\n".join(animal_list)
+    return f"<ul>ID: {id}</ul><ul>Environment: {enclosure.environment}</ul><ul>Open to Visitors: {enclosure.open_to_visitors}</ul>{formatted_list}"
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
